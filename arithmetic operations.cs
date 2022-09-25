867e38ee650a816ac31c61858cf0f45a448ee377
mimmarry <105546871+mimmarry@users.noreply.github.com>
2022-09-25 19:22:34 +0300

    Add files via upload

---
+++ arithmetic operations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace gg;
+class Programm
+ {static void Main(string[] args)
+    {
+        Console.WriteLine("Vvedi chislo");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        int result = a - 48;
+        if (result > 48)
+        {
+            result = (a - 48) * 3;
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine(result);
+        }
+        Console.ReadLine();
+
+    }
+
+}
